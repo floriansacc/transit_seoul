04a3e3196add2dc099d693409c5e039281a5a967
floriansacc <florian.sacchetti@gmail.com>
2025-01-26 00:53:31 +0900

    ♻️ separate bus search

--- lib/pages/bus/bus_info_page.dart
+++ lib/pages/bus/bus_info_page.dart
@@ -1,13 +1,11 @@
 import 'package:transit_seoul/components/confirm_button.dart';
-import 'package:transit_seoul/components/custom_search_bar.dart';
-import 'package:transit_seoul/components/custom_text_form_field.dart';
 import 'package:transit_seoul/controllers/public_method.dart';
 import 'package:transit_seoul/pages/bus/components/bus_details.dart';
 import 'package:transit_seoul/pages/bus/components/bus_map.dart';
+import 'package:transit_seoul/pages/bus/components/bus_search.dart';
 import 'package:transit_seoul/providers/bus_info_cubit/bus_info_cubit.dart';
 import 'package:transit_seoul/styles/style_text.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gap/gap.dart';
 
@@ -24,10 +22,8 @@ class BusInfoPage extends StatefulWidget {
 }
 
 class _BusInfoPageState extends State<BusInfoPage> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final ScrollController scrollController = ScrollController();
 
-  TextEditingController searchController = TextEditingController();
   FocusNode searchFocusNode = FocusNode();
 
   final ValueNotifier<bool> displayActions = ValueNotifier<bool>(false);
@@ -50,32 +46,11 @@ class _BusInfoPageState extends State<BusInfoPage> {
   @override
   void dispose() {
     shouldDrawLine.dispose();
+    searchFocusNode.dispose();
     isMapFullScreen.dispose();
     displayActions.dispose();
-    searchFocusNode.dispose();
     scrollController.dispose();
-    searchController.dispose();
     super.dispose();
-  }
-
-  Future<void> validateSearch() async {
-    shouldDrawLine.value = false;
-    BusInfoCubit busCubit = context.read<BusInfoCubit>();
-    if (_formKey.currentState!.validate()) {
-      await busCubit.getBusRouteInfo(
-        int.parse(searchController.value.text),
-        getDetails: true,
-      );
-      if (busCubit.state.status.isSuccess) {
-        shouldDrawLine.value = true;
-      }
-    }
-    if (!mounted) throw Exception();
-
-    if (context.read<BusInfoCubit>().state.busInfo != null) {
-      searchController.clear();
-      FocusManager.instance.primaryFocus?.unfocus();
-    }
   }
 
   void openSearchModal() {
@@ -86,7 +61,7 @@ class _BusInfoPageState extends State<BusInfoPage> {
         spacing: 12,
         mainAxisSize: MainAxisSize.min,
         children: [
-          fieldRow(),
+          BusSearch(shouldDrawLine: shouldDrawLine),
           ConfirmButton(
             description: '닫기',
             onTap: () => Navigator.of(context).pop(),
@@ -137,80 +112,42 @@ class _BusInfoPageState extends State<BusInfoPage> {
               ],
               scrolledUnderElevation: 10,
             ),
-            body: Form(
-              key: _formKey,
-              child: ValueListenableBuilder(
-                valueListenable: isMapFullScreen,
-                builder: (context, isFullScreen, child) =>
-                    SingleChildScrollView(
-                  controller: scrollController,
-                  child: Column(
-                    spacing: 12,
-                    children: [
-                      if (!isFullScreen) ...[
-                        fieldRow(focusNode: searchFocusNode),
-                        Hero(
-                          tag: widget.heroTag ?? '',
-                          child: Image.asset('assets/images/test_1.avif'),
-                        ),
-                        BusDetails(),
-                      ],
-                      BusMap(
+            body: ValueListenableBuilder(
+              valueListenable: isMapFullScreen,
+              builder: (context, isFullScreen, child) => SingleChildScrollView(
+                controller: scrollController,
+                child: Column(
+                  spacing: 12,
+                  children: [
+                    if (!isFullScreen) ...[
+                      BusSearch(
                         shouldDrawLine: shouldDrawLine,
-                        isMapFullScreen: isMapFullScreen,
+                        focusNode: searchFocusNode,
                       ),
-                      Gap(isFullScreen ? 0 : 150),
+                      Hero(
+                        tag: widget.heroTag ?? '',
+                        child: Image.asset('assets/images/test_1.avif'),
+                      ),
+                      BusDetails(),
                     ],
-                  ),
+                    BusMap(
+                      shouldDrawLine: shouldDrawLine,
+                      isMapFullScreen: isMapFullScreen,
+                    ),
+                    Gap(isFullScreen ? 0 : 150),
+                  ],
                 ),
               ),
             ),
           ),
         ),
-        PublicMethod.nextButtonIosKeyboard(
-          context,
-          displayCondition: searchFocusNode.hasFocus,
-          onTapFuction: () => FocusManager.instance.primaryFocus?.unfocus(),
-          buttonText: '닫기',
-        ),
+        // PublicMethod.nextButtonIosKeyboard(
+        //   context,
+        //   displayCondition: searchFocusNode.hasFocus,
+        //   onTapFuction: () => FocusManager.instance.primaryFocus?.unfocus(),
+        //   buttonText: '닫기',
+        // ),
       ],
-    );
-  }
-
-  Padding fieldRow({FocusNode? focusNode}) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        spacing: 12,
-        children: [
-          CustomSearchBar(
-            textFormField: CustomTextFormField(
-              focusNode: focusNode,
-              controller: searchController,
-              hintText: '버스 번호',
-              textInputAction: TextInputAction.done,
-              keyboardType: TextInputType.number,
-              masks: [
-                FilteringTextInputFormatter.digitsOnly,
-              ],
-              onChange: (_) {
-                if (searchController.text.length == 1) {
-                  _formKey.currentState!.validate();
-                }
-              },
-              validator: (String? value) {
-                {
-                  if (value == null || value.isEmpty) {
-                    return '버스 번호를 입력하세요.';
-                  }
-                  return null;
-                }
-              },
-            ),
-            onTapSearch: () async => validateSearch(),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/pages/bus/components/bus_search.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:transit_seoul/components/custom_search_bar.dart';
+import 'package:transit_seoul/components/custom_text_form_field.dart';
+import 'package:transit_seoul/providers/bus_info_cubit/bus_info_cubit.dart';
+
+class BusSearch extends StatefulWidget {
+  const BusSearch({
+    super.key,
+    required this.shouldDrawLine,
+    this.focusNode,
+  });
+
+  final ValueNotifier<bool> shouldDrawLine;
+  final FocusNode? focusNode;
+
+  @override
+  State<BusSearch> createState() => _BusSearchState();
+}
+
+class _BusSearchState extends State<BusSearch> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
+  Future<void> validateSearch() async {
+    widget.shouldDrawLine.value = false;
+    BusInfoCubit busCubit = context.read<BusInfoCubit>();
+    if (_formKey.currentState!.validate()) {
+      await busCubit.getBusRouteInfo(
+        int.parse(searchController.value.text),
+        getDetails: true,
+      );
+      if (busCubit.state.status.isSuccess) {
+        widget.shouldDrawLine.value = true;
+      }
+    }
+    if (!mounted) throw Exception();
+
+    if (context.read<BusInfoCubit>().state.busInfo != null) {
+      searchController.clear();
+      FocusManager.instance.primaryFocus?.unfocus();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          spacing: 12,
+          children: [
+            CustomSearchBar(
+              textFormField: CustomTextFormField(
+                focusNode: widget.focusNode,
+                controller: searchController,
+                hintText: '버스 번호',
+                textInputAction: TextInputAction.done,
+                keyboardType: TextInputType.number,
+                masks: [
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                onChange: (_) {
+                  if (searchController.text.length == 1) {
+                    _formKey.currentState!.validate();
+                  }
+                },
+                validator: (String? value) {
+                  {
+                    if (value == null || value.isEmpty) {
+                      return '버스 번호를 입력하세요.';
+                    }
+                    return null;
+                  }
+                },
+              ),
+              onTapSearch: () async => validateSearch(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
